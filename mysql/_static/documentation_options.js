80e446b23385e46f3440e5a85b544ae71e2a831d
david-erb <david-erb@users.noreply.github.com>
2023-05-26 06:59:45 +0000

    deploy: 48b658be8ec9b737c21278439bfae17a3c68f317

--- mysql/_static/documentation_options.js
+++ mysql/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.2.3.dev17+g7a22466',
+    VERSION: '1.2.3.dev18+g48b658b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
